03eb76955ffb567e0f4e452ba00db2f5f96b57e5
hnbtram <baokhanh20002701@gmail.com>
2025-07-29 09:38:18 +0000

    framework

--- QuanNetTramOanh/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ QuanNetTramOanh/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -11,6 +11,13 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
+		System.out.println("Greeting request received with name: " + name);
+		return "greeting";
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(name="hovaten", required=false, defaultValue="Ho va ten") String hovaten, Model model) {
+		model.addAttribute("name", hovaten);
+		System.out.println("Hello request received with name: " + hovaten);
 		return "greeting";
 	}
 
