ecb287671af58259a3fd98fce00cc836a380afc6
hnbtram <baokhanh20002701@gmail.com>
2025-08-11 15:54:15 +0000

    tram

--- QuanNetTramOanh/complete/src/test/java/com/example/servingwebcontent/App.java
+++ QuanNetTramOanh/complete/src/test/java/com/example/servingwebcontent/App.java
@@ -1,37 +1,13 @@
 package com.example.servingwebcontent;
 public class App {
     public static void main(String[] args) throws Exception {
-     //QuanLyKhachHangTest test = new QuanLyKhachHangTest();
-        //test.setUp();
-        //test.testThemKhachHang();
-        //test.testXoaKhachHang();
-        //test.testTimKhachHang();
-        //test.testNapTien();
-        //test.testGetDanhSachKhachHang();
-        //test.testLichSuDichVu();
-    //QuanLyMayTinhTest test = new QuanLyMayTinhTest();
-        //test.setUp();
-        //test.testThemMayTinh();
-        //test.testTimMayTinh();
-        //test.testCapNhatGia();
-        //test.testBatDauSuDung();
-        //test.testKetThucSuDung();
-        //test.testXoaMayTinh();
+     
+    QuanLyMayTinhTest.testQuanLyMayTinh();
+    QuanLyKhachHangTest.testQuanLyKhachHang();
+    QuanLyThanhToanTest.testQuanLyThanhToan();
 
-        //DichVuServiceTest test = new DichVuServiceTest();
-        //test.setUp();
-        //test.themDichVu_ThanhCong();
-        //test.layTatCaDichVu_CoDuLieu();
-        //test.timTheoMa_TonTai();
-        //test.timTheoMa_KhongTonTai();
-        //test.capNhatDichVu_ThanhCong();
-        //test.capNhatDichVu_KhongTonTai();
-        //test.xoaDichVu_ThanhCong();
-        //test.xoaDichVu_KhongTonTai();
-        //test.layDichVuDangHoatDong();
         
-        QuanLyMayTinhTest.testQuanLyMayTinh();
-
+        
 
     }
 }
